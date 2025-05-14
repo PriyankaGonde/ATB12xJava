989af09e2551426e164768f013c6bb01b5fa8842
PriyankaGonde <PriyankaGonde@github.com>
2025-05-14 19:57:52 +0530

    test chages

--- src/test/java/Task1.java
+++ src/test/java/Task1.java
@@ -1,8 +1,8 @@
 public class Task1 {
     public static void main(String[] args) {
-        int num = 5;
+        int num = 6;
         char new_line='\n';
-
+        // test change
         for (int i = 1; i <= 10; i++) {
             System.out.printf(new_line+"%d", num * i);
         }
