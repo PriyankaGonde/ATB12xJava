65cce9c01b9e6221816d23f8abfe448bf61726c7
PriyankaGonde <PriyankaGonde@github.com>
2025-05-14 19:59:45 +0530

    revert

--- src/test/java/Task1.java
+++ src/test/java/Task1.java
@@ -1,6 +1,6 @@
 public class Task1 {
     public static void main(String[] args) {
-        int num = 6;
+        int num = 5;
         char new_line='\n';
         // test change
         for (int i = 1; i <= 10; i++) {
