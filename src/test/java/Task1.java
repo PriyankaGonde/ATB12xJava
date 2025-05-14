c40a4a733b9a9e50f9bd83b90aac42bee15f2d4e
Priyanka Gonde <priyankagonde@Priyankas-MacBook-Air.local>
2025-05-14 19:46:08 +0530

    first commit

---
+++ src/test/java/Task1.java
@@ -0,0 +1,10 @@
+public class Task1 {
+    public static void main(String[] args) {
+        int num = 5;
+        char new_line='\n';
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf(new_line+"%d", num * i);
+        }
+    }
+}
